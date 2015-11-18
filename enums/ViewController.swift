3a5e14d3da4bfbe6b44c80135f38add11fb93255
jeffrey <jeffcho59@gmail.com>
2015-11-17 19:26:53 -0500

    added enum files

--- enums/ViewController.swift
+++ enums/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var theLabel: UILabel!
+    enum Cars: Int {
+        case BMW = 0
+        case HONDA = 1
+        case TESLA = 2
+        case SUBARU = 3
+    }
+    
+    var carOfChoice: Cars!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+
     }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onBtnTapped(sender: AnyObject) {
+        if sender.tag == Cars.BMW.rawValue {
+            theLabel.text = "BMW us awesome!"
+        } else if sender.tag == Cars.HONDA.rawValue {
+            theLabel.text = "Honda's are okay"
+        } else if sender.tag == Cars.TESLA.rawValue {
+            theLabel.text = "Teslas are really new!"
+        } else {
+            theLabel.text = "Get different rims"
+        }
+    }
 }
+
 
